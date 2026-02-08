e7095d71137cf98b561f6298e01fd5af671a3ce1
shaik17malik <shaik17malik@gmail.com>
2026-02-08 07:53:40 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,8 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+
+print("addition = ", a + b)
+print("substraction = ", a - b)
+print("multiplication= ", a * b)
+print("Division = ", a / b)
+print("good you are completely with your first project")
